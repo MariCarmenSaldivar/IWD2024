3586e9a1313325349f7e4cf3d2efae074124f2fd
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-01 14:06:32 -0500

    test Azure configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,22 @@
           name = "iwd-test-us-east"
         }
       }
+      required_providers {
+      azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.7.0"
     }
+  }
+  }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "eastus"
+  name     = "iwd2024"
+}
 
     # An example resource that does nothing.
     resource "null_resource" "example" {
