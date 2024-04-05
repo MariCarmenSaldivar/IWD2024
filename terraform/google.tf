c27834ef48d1b09de0088fe04b8c602892bb4b0e
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-05 17:13:16 -0500

    fix cloud run v2

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,31 +1,31 @@
 resource "google_cloud_run_v2_service" "default" {
   name     = "iwd-cloudrun-srv"
   location = "us-central1"
-  ingress = "INGRESS_TRAFFIC_ALL"
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
-      containers {
-        image = "index.docker.io/msaldivar/iwdimgdemo:main"
-      }
+    containers {
+      image = "index.docker.io/msaldivar/iwdimgdemo:main"
+    }
   }
 
   traffic {
-    percent         = 100
-    latest_revision = true
+    percent = 100
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
   }
 }
 
- data "google_iam_policy" "noauth" {
-   binding {
-     role = "roles/run.invoker"
-     members = ["allUsers"]
-   }
- }
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
 
- resource "google_cloud_run_service_iam_policy" "noauth" {
-   location    = google_cloud_run_v2_service.default.location
-   project     = google_cloud_run_v2_service.default.project
-   service     = google_cloud_run_v2_service.default.name
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  project  = google_cloud_run_v2_service.default.project
+  service  = google_cloud_run_v2_service.default.name
 
-   policy_data = data.google_iam_policy.noauth.policy_data
- }
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
