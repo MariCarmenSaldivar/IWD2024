47538263b988af97a000f85bf78ac4b18b285106
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-05 17:04:19 -0500

    adding public access

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,5 +1,5 @@
-resource "google_cloud_run_service" "default" {
-  name     = "cloudrun-srv"
+resource "google_cloud_run_v2_service" "default" {
+  name     = "iwd-cloudrun-srv"
   location = "us-central1"
 
   template {
@@ -15,3 +15,18 @@ resource "google_cloud_run_service" "default" {
     latest_revision = true
   }
 }
+
+ data "google_iam_policy" "noauth" {
+   binding {
+     role = "roles/run.invoker"
+     members = ["allUsers"]
+   }
+ }
+
+ resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_v2_service.default.location
+   project     = google_cloud_run_v2_service.default.project
+   service     = google_cloud_run_v2_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+ }
