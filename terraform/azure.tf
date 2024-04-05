04449db832435c30459650be02d49b7e8f8bb6a8
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-04 22:10:34 -0500

    fix terraform

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -7,6 +7,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "iwd2024"
 }
 
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
