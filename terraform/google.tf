6f253db273523c1dee3b0bd4666ab54f976a5297
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-05 17:19:40 -0500

    change configuration to public

--- terraform/google.tf
+++ terraform/google.tf
@@ -15,17 +15,11 @@ resource "google_cloud_run_v2_service" "default" {
   }
 }
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role    = "roles/run.invoker"
-    members = ["allUsers"]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
+resource "google_cloud_run_service_iam_binding" "default" {
   location = google_cloud_run_v2_service.default.location
-  project  = google_cloud_run_v2_service.default.project
   service  = google_cloud_run_v2_service.default.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
 }
