9b7323352585de9e078a5c22c934c6e20c97fc29
CarmenSaldivar <maricarmen.saldivar@gmail.com>
2024-04-05 18:02:50 -0500

    Update docker file

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -12,28 +12,3 @@ resource "random_integer" "ri" {
   min = 10000
   max = 99999
 }
-
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
-
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.appserviceplan.id
-  https_only          = true
-  site_config {
-    minimum_tls_version = "1.2"
-    application_stack {
-      docker_image_name        = "msaldivar/personalsite:latest"
-      docker_registry_url      = "https://index.docker.io"
-    }
-  }
-}
--- terraform/google.tf
+++ terraform/google.tf
@@ -15,11 +15,11 @@ resource "google_cloud_run_v2_service" "default" {
   }
 }
 
-resource "google_cloud_run_service_iam_binding" "default" {
+resource "google_cloud_run_service_iam_binding" "binding" {
   location = google_cloud_run_v2_service.default.location
   service  = google_cloud_run_v2_service.default.name
   role     = "roles/run.invoker"
   members = [
     "allUsers"
-  ]
+  ] 
 }
