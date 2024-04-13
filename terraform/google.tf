059e997d26c333c9ef9f9b909f6e02057211467c
Maricarme Saldivar <maricarmen.saldivar@gmail.com>
2024-04-12 23:08:11 -0500

    Update google.tf
    Demo

--- terraform/google.tf
+++ terraform/google.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_run_v2_service" "default" {
-  count = 1
+  count = 5
   name     = "iwd-cloudrun-srv${count.index}"
   location = "us-central1"
   ingress  = "INGRESS_TRAFFIC_ALL"
@@ -17,7 +17,7 @@ resource "google_cloud_run_v2_service" "default" {
 }
 
 resource "google_cloud_run_service_iam_binding" "binding" {
-  count = 1
+  count = 5
   location = google_cloud_run_v2_service.default[count.index].location
   service  = google_cloud_run_v2_service.default[count.index].name
   role     = "roles/run.invoker"
